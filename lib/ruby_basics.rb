489551859d412479dc025c38ca030b995bc6da00
Ethan Ryan <ethanryan@gmail.com>
2017-02-23 19:35:21 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+  num1 / num2 #divide num1 by num2 and return result
 end
 
-def assign_variable(value)
-  
+def assign_variable(value) #value is the argument
+  #puts value #outputs value (to see what i'm getting)
+  value #value is returned
 end
 
-def argue
-  
+def argue(argument) #argument is the argument
+  argument #argument is returned
 end
 
-def greeting
-
+def greeting(argument1, argument2) #method takes two arguments, argument1 and argument2
+  #no need to put anything here, tests pass
 end
 
-def return_a_value
-
+def return_a_value #no argument
+  return "Nice" #returns the string "Nice"
 end
 
-def last_evaluated_value
-
+def last_evaluated_value #no argument
+  return "expert" #returns the string "expert"
 end
 
-def pizza_party
-
+def pizza_party(argument="cheese") #argument is the argument, default argument is "cheese"
+  return argument #returns argument
 end
